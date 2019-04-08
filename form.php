bc537c945881dc0a76abc959d25b2b8ddbeb594c
Samai.Duch <samai.duch@PNC.LAN>
2019-04-08 10:52:02 +0700

    stylefile

---
+++ form.php
@@ -0,0 +1,40 @@
+
+<div class="title">
+ <h1 class="text-center text-white">WEB CALCULATOR</h1>
+</div>
+<div class="row">
+ <div class="col-3"></div>
+ <div class="col-6">
+    <div class="card text-white ">
+     <div class="card-body">
+        <form action="#" method="POST">
+            <div class="form-group">
+              <span>First Number</span>
+               <input type="number" name="firstnumber" id="" class="form-control" required>
+            </div>
+            <div class="form-group">
+              <span>Select Operator</span>
+                <select name="select" id="" class="form-control">
+                   <option value="Non">None</option>
+                   <option value="Minus">Minus</option>
+                   <option value="Multiply">Multiply</option>
+                   <option value="Divide">Divide</option>
+                </select>
+            </div>
+            <div class="form-group">
+              <span>Second Number</span>
+              <input type="number" name="secondnumber" id="" class="form-control " required>
+                
+            </div>
+            <div class="form-group">
+              <button type="submit" class="btn bg-info text-white">Calculate</button>
+                
+            </div>
+            <h5 > The result is : 0</h3>
+        </form>
+
+       
+     </div>
+    </div>
+ </div>
+</div>
